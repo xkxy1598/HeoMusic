25bf9ee790cec4dc502644e9d4fbfadd4f5d54d9
xkxy1598 <130194265+xkxy1598@users.noreply.github.com>
2025-08-05 14:38:51 +0800

    Update and rename config.js.demo to config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-var userId = "8668419170";
-var userServer = "tencent";
+var userId = "5152895647";
+var userServer = "netease";
 var userType = "playlist";
 // var localMusic = [{
 //     name: '重生之我在异乡为异客',
